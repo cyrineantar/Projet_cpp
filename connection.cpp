fcfb5dfa27696f39cec1d4e061554ef1543fb409
Cyrine Antar <cyrine.antar@esprit.tn>
2020-11-26 23:32:29 +0100

    first commit

---
+++ connection.cpp
@@ -0,0 +1,22 @@
+#include "connection.h"
+#include <QDebug>
+
+Connection::Connection()
+{
+
+}
+
+bool Connection::createconnect()
+{bool test=false;
+QSqlDatabase db = QSqlDatabase::addDatabase("QODBC");
+db.setDatabaseName("Source_Projet2A");
+db.setUserName("Cyrine");//inserer nom de l'utilisateur
+db.setPassword("cyrine1");//inserer mot de passe de cet utilisateur
+
+if (db.open())
+test=true;
+qDebug()<<db.lastError().text();
+
+
+    return  test;
+}
